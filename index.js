962a0582755e6be917f7a429a81528224cd7950e
Vidip Ghosh <vidip2001@gmail.com>
2024-04-06 11:39:07 +0530

    chore: ghw

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const axios = require("axios");
+const cheerio = require("cheerio");
+const port = 3000;
+
+app.get("/", async (req, res) => {
+  res.json({ message: " Welcome to Web Scraping API" });
+});
+
+app.get("/challenges", async (req, res) => {
+  axios.get("https://ghw.mlh.io/challenges").then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+    $('a.contains("")', html).each(function () {
+      const title = $(this).text();
+    });
+    res.json(challenges);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
